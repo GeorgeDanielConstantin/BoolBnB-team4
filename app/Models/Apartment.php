d5e3a85e95a76b700c0f5ef715b64b6cc31e3acd
DanieleMarino70 <danielaotitan@gmail.com>
2023-05-15 10:57:48 +0200

    add: image getter

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -9,24 +9,36 @@ class Apartment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ["title", "description", "address", "latitude", "longitude", "rooms", "bathrooms","beds","square_meters"];
+    protected $fillable = ["title", "description", "image", "address", "latitude", "longitude", "rooms", "bathrooms", "beds", "square_meters"];
 
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function message() {
+    public function message()
+    {
         return $this->hasMany(Message::class);
     }
-    public function apartmentsponsor() {
+    public function apartmentsponsor()
+    {
         return $this->hasMany(ApartmentSponsor::class);
     }
 
-    public function views() {
+    public function views()
+    {
         return $this->hasMany(View::class);
     }
 
-    public function service() {
+    public function service()
+    {
         return $this->belongsMany(Service::class);
+    }
+
+
+    // GETTERS
+    public function getImageUri()
+    {
+        return $this->image ? url('storage/' . $this->image) : 'https://www.frosinonecalcio.com/wp-content/uploads/2021/09/default-placeholder.png';
     }
 }
