1648c5b7967a7f66a67007db122bd8c142abd3b8
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-05-15 10:58:16 +0200

    fix apartments owners

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,12 +15,52 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // $user = new User;
+        // $user->name = 'admin';
+        // $user->surname = 'admin';
+        // $user->email = 'admin@mail.it';
+        // $user->password = 'admin';
+        // $user->date_of_birth = '2000-01-01';
+        // $user->save();
+
         $user = new User;
-        $user->name = 'admin';
-        $user->surname = 'admin';
-        $user->email = 'admin@mail.it';
+        $user->name = 'Paolo';
+        $user->surname = 'Cardinale';
+        $user->email = 'alfio09@hotmail.com';
         $user->password = 'admin';
-        $user->date_of_birth = '2000-01-01';
+        $user->date_of_birth = '1981-01-01';
+        $user->save();
+
+        $user = new User;
+        $user->name = 'Daniele';
+        $user->surname = 'Marino';
+        $user->email = 'daniele.noneporno@sesso.com';
+        $user->password = 'admin';
+        $user->date_of_birth = '1998-01-01';
+        $user->save();
+
+        $user = new User;
+        $user->name = 'Simone';
+        $user->surname = 'Lombardi';
+        $user->email = 'samuraikkonen@ludopatia.com';
+        $user->password = 'admin';
+        $user->date_of_birth = '2002-01-01';
+        $user->save();
+
+        $user = new User;
+        $user->name = 'Riccardo';
+        $user->surname = 'Pietrobon';
+        $user->email = 'riccardospiller@nikolai.ru';
+        $user->password = 'admin';
+        $user->date_of_birth = '1996-01-01';
+        $user->save();
+    
+        $user = new User;
+        $user->name = 'Daniel';
+        $user->surname = 'Constantin';
+        $user->email = 'danielgconstantin@gmail.com';
+        $user->password = 'admin';
+        $user->date_of_birth = '1997-01-01';
         $user->save();
     }
 }
