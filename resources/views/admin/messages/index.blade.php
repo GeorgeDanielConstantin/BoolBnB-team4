0abeb34c54b2488623319c45e48cfcc2fee4639b
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-05-19 15:51:23 +0200

    fix apartment seeder

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -43,3 +43,30 @@
 </section>
 
 @endsection
+
+{{-- modale --}}
+@section('modals')
+    @foreach ($messages as $message)
+        <div class="modal fade" id="delete-message-modal-{{ $message->id }}" tabindex="-1" aria-labelledby="delete-message-modal-{{ $message->id }}-label" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="delete-message-modal-{{ $message->id }}-label">Elimina l'appartamento n°{{ $message->id }}</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    L'appartamento <strong>{{ $message->title }}</strong> sarà eliminato. Sei sicuro?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No, annulla</button>
+                    <form method="POST" action="{{ route('admin.messages.destroy', $message)}}">
+                        @method('delete')
+                        @csrf
+                        <button type="submit" class="btn btn-primary">Sì, elimina</button>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+@endsection
