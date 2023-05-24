fa8c9eca2bd031ac3ad28fe8ea3408fb357b6e04
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-05-24 12:59:30 +0200

    sponsor additional

--- app/Http/Controllers/Admin/SponsorController.php
+++ app/Http/Controllers/Admin/SponsorController.php
@@ -1,94 +1,79 @@
-<?php
-
 namespace App\Http\Controllers\Admin;
+
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
-use App\Models\Sponsor;
 use App\Models\ApartmentSponsor;
 use Illuminate\Http\Request;
 use Braintree\Gateway;
-use Spatie\LaravelIgnition\Recorders\DumpRecorder\Dump;
 use Carbon\Carbon;
 use App\Jobs\UpdateVisibilityJob;
 
 class SponsorController extends Controller
 {
-  
-    public function showSponsorshipForm(Apartment $apartment)
+public function showSponsorshipForm(Apartment $apartment)
 {
-    // Verifica se l'utente autenticato è il proprietario dell'appartamento
-    if ($apartment->user_id !== auth()->id()) {
-        return redirect()->back()->with('error', 'Accesso non autorizzato');
-    }
-
-    // L'utente è autorizzato, visualizza il modulo di sponsorizzazione
-    return view('admin.sponsorship.form', compact('apartment'));
+// Verifica se l'utente autenticato è il proprietario dell'appartamento
+if ($apartment->user_id !== auth()->id()) {
+return redirect()->back()->with('error', 'Accesso non autorizzato');
 }
 
-
+// L'utente è autorizzato, visualizza il modulo di sponsorizzazione
+return view('admin.sponsorship.form', compact('apartment'));
+}
 
 public function processSponsorship(Request $request, Apartment $apartment)
 {
-    $sponsorshipType = $request->input('sponsorship_type');
-    $amount = 0;
+$sponsorshipType = $request->input('sponsorship_type');
+$amount = 0;
 
-    // Imposta l'importo in base al tipo di sponsorizzazione
-    if ($sponsorshipType === 'basic') {
-        $amount = 2.99;
-        $visibilityDuration = 30; // Durata in secondi per la sponsorizzazione basic
-    } elseif ($sponsorshipType === 'standard') {
-        $amount = 5.99;
-        $visibilityDuration = 72 * 3600; // Durata in secondi per la sponsorizzazione standard (72 ore)
-    } elseif ($sponsorshipType === 'premium') {
-        $amount = 9.99;
-        $visibilityDuration = 144 * 3600; // Durata in secondi per la sponsorizzazione premium (144 ore)
-    }
+// Imposta l'importo in base al tipo di sponsorizzazione
+if ($sponsorshipType === 'basic') {
+$amount = 2.99;
+$visibilityDuration = 30; // Durata in secondi per la sponsorizzazione basic
+} elseif ($sponsorshipType === 'standard') {
+$amount = 5.99;
+$visibilityDuration = 72 * 3600; // Durata in secondi per la sponsorizzazione standard (72 ore)
+} elseif ($sponsorshipType === 'premium') {
+$amount = 9.99;
+$visibilityDuration = 144 * 3600; // Durata in secondi per la sponsorizzazione premium (144 ore)
+}
 
-    $gateway = new Gateway([
-        'environment' => env('BRAINTREE_ENV'),
-        'merchantId' =>  env('BRAINTREE_MERCHANT_ID'),
-        'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
-        'privateKey' => env('BRAINTREE_PRIVATE_KEY')
-    ]);
+$gateway = new Gateway([
+'environment' => env('BRAINTREE_ENV'),
+'merchantId' => env('BRAINTREE_MERCHANT_ID'),
+'publicKey' => env('BRAINTREE_PUBLIC_KEY'),
+'privateKey' => env('BRAINTREE_PRIVATE_KEY')
+]);
 
-    $result = $gateway->transaction()->sale([
-        'amount' => $amount, // Importo da addebitare
-        'paymentMethodNonce' => 'nonce-from-the-client',
-        'options' => [
-            'submitForSettlement' => true
-        ]
-    ]);
+$result = $gateway->transaction()->sale([
+'amount' => $amount, // Importo da addebitare
+'paymentMethodNonce' => 'nonce-from-the-client',
+'options' => [
+'submitForSettlement' => true
+]
+]);
 
-    if ($result->success) {
-        // Pagamento riuscito
+if ($result->success) {
+// Pagamento riuscito
 
-        $apartmentSponsor = new ApartmentSponsor();
-        $apartmentSponsor->starting_date = now();
+$existingSponsorship = $apartment->apartmentsponsor()->where('ending_date', '>', now())->latest()->first();
 
-        if ($sponsorshipType === 'basic') {
-            $expiration = now()->addSeconds($visibilityDuration);
-        } elseif ($sponsorshipType === 'standard') {
-            $expiration = now()->addSeconds($visibilityDuration);
-        } elseif ($sponsorshipType === 'premium') {
-            $expiration = now()->addSeconds($visibilityDuration);
-        }
-
-        $apartmentSponsor->ending_date = $expiration;
-
+if ($existingSponsorship) {
+// Estendi la sponsorizzazione esistente
+$existingSponsorship->ending_date = $existingSponsorship->ending_date->addSeconds($visibilityDuration);
+$existingSponsorship->save();
+} else {
+// Crea una nuova sponsorizzazione
+$apartmentSponsor = new ApartmentSponsor();
+$apartmentSponsor->starting_date = now();
+$apartmentSponsor->ending_date = now()->addSeconds($visibilityDuration);
 $apartment->apartmentsponsor()->save($apartmentSponsor);
+}
 
 $apartment->visibility = true;
 $apartment->save();
 
-$delayInSeconds = now()->diffInSeconds($expiration);
+$delayInSeconds = now()->diffInSeconds($apartmentSponsor->ending_date);
 UpdateVisibilityJob::dispatch($apartmentSponsor)->delay($delayInSeconds);
-        return redirect()->route('admin.apartments.show', $apartment)->with('success', 'Pagamento effettuato con successo!');
-    } else {
-        // Pagamento fallito
-        return redirect()->back()->with('error', 'Pagamento fallito. Riprova.');
-    }
-}
 
-
-
-}
+return redirect()->route('admin.apartments.show', $apartment)->with('success', 'Pagamento
